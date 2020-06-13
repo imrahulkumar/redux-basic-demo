99df92d8e1950a4f439fbefa4c39609531b1f2a3
Rahul kumar <rahulkumar151995@gmail.com>
2020-06-14 01:20:33 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import './App.css'
+import store from './redux/store'
+import CakeContainer from './components/CakeContainer'
+import HooksCakeContainer from './components/HooksCakeContainer'
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className='App'>
+        <CakeContainer />
+        <HooksCakeContainer />
+      </div>
+    </Provider>
+  )
+}
+
+export default App
